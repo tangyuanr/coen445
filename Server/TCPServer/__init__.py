f4b3a6a5eda9109f577a89c937ca395c36dd4a72
ThierrySt-Arnaud <haratiri@gmail.com>
2018-11-09 00:27:44 -0500

    UDP server changed to quit properly on Ctrl-C. TCP server multithread. Need to write test program.

--- Server/TCPServer/__init__.py
+++ Server/TCPServer/__init__.py
@@ -1,32 +1,122 @@
-#! /usr/bin/python
-
 import socket
+import Queue
+import threading
 import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-# Server starts listening on the port
-sock.listen(1)
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    try:
-        print >>sys.stderr, 'connection from', client_address
+__MAXSIZE__ = 1024
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(1024)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
+
+class TCPServer:
+    """docstring for TCPServer."""
+
+    def __init__(self, HOST, PORT, msgQueue, MAXSIZE=1024):
+        self.HOST = HOST
+        self.PORT = PORT
+        self.recvQueue = msgQueue
+        self.connections = []
+
+        global __MAXSIZE__
+        __MAXSIZE__ = MAXSIZE
+
+        try:
+            self.serverSckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error, msg:
+            print >>sys.stderr, 'Failed to create socket. Error Code : '\
+                                + str(msg[0]) + ' Message ' + msg[1]
+            raise
+        try:
+            self.serverSckt.bind((HOST, PORT))
+        except socket.error, msg:
+            print >>sys.stderr, 'Bind failed. Error Code : '\
+                                + str(msg[0]) + ' Message ' + msg[1]
+            raise
+
+        self.TCPLstnr = TCPLstnr('Listener', self.serverSckt,
+                                 self.recvQueue, self.connections)
+        self.TCPLstnr.start()
+
+    def sendall(self, msg):
+            for c in self.connections:
+                c.send(msg)
+
+    def disconnect(self, connection):
+        connection.disconnect()
+        connection.join()
+        self.connections.remove(connection)
+
+    def shutdown(self):
+        self.TCPLstnr.stop()
+        for t in self.connections:
+            t.disconnect()
+        while len(threads) > 0:
+            threads = [t.join(1) for t in threads
+                       if t is not None and t.isAlive()]
+
+
+class TCPLstnr(threading.Thread):
+    """Thread listening to new connections.
+    Appends new connections to connections"""
+
+    def __init__(self, name, serverSckt, recvQueue, connections):
+        super(TCPLstnr, self).__init__()
+        self.name = name
+        self.serverSckt = serverSckt
+        self.recvQueue = recvQueue
+        self.connections = connections
+        self.killFlag = threading.Event()
+
+    def run(self):
+        while not self.killFlag.isSet():
+            try:
+                self.serverSckt.listen(5)
+                clientSckt, address = self.serverSckt.accept()
+                connection = TCPCnxn(str(address), clientSckt,
+                                     self.recvQueue)
+                connection.start()
+                self.connections.append(connection)
+            except socket.error:
                 break
 
-    finally:
-        # Clean up the connection
-        connection.close()
+    def stop(self):
+        self.killFlag.set()
+        self.serverSckt.shutdown(SHUT_RDWR)
+
+
+class TCPCnxn(threading.Thread):
+    """Thread listening to connected endpoint"""
+
+    def __init__(self, name, clientSckt, recvQueue):
+        super(TCPCnxn, self).__init__()
+        self.name = name
+        self.clientSckt = clientSckt
+        self.recvQueue = recvQueue
+        self.killFlag = threading.Event()
+
+    def run(self):
+        while not self.killFlag.isSet():
+            try:
+                msg = [self]
+                msg.append(self.clientSckt.recv(__MAXSIZE__))
+                self.recvQueue.put(msg)
+            except socket.error, error:
+                print >>sys.stderr, "Socket error while receiving. Code: "\
+                                    + str(error[0]) + "Message: " + error[1]
+                break
+        self.clientSckt.close()
+
+    def send(self, msg):
+        try:
+            self.clientSckt.sendall(msg)
+            break
+        except socket.error, error:
+            print >>sys.stderr, "Socket error while sending. Code: "\
+                                + str(error[0]) + "Message: " + error[1]
+            raise
+
+    def disconnect(self):
+        self.killFlag.set()
+        self.clientSckt.shutdown(SHUT_WR)
+
+
+if __name__ == '__main__':
+    
--- Server/UDPServer/__init__.py
+++ Server/UDPServer/__init__.py
@@ -16,7 +16,6 @@ class UDPServer:
         self.PORT = PORT
         self.killFlag = threading.Event()
         self.recvQueue = msgQueue
-        self.sendQueue = Queue.Queue()
 
         global __MAXSIZE__
         __MAXSIZE__ = MAXSIZE
@@ -25,45 +24,39 @@ class UDPServer:
             self.sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.sckt.settimeout(0.1)
         except socket.error, msg:
-            print 'Failed to create socket. Error Code : '\
+            print >>sys.stderr, 'Failed to create socket. Error Code : '\
                   + str(msg[0]) + ' Message ' + msg[1]
             raise
         try:
             self.sckt.bind((HOST, PORT))
         except socket.error, msg:
-            print 'Bind failed. Error Code : ' + str(msg[0])\
-                  + ' Message ' + msg[1]
+            print >>sys.stderr, 'Bind failed. Error Code : ' + str(msg[0])\
+                                + ' Message ' + msg[1]
             raise
 
-        threadNames = ['RCVR', 'SNDR']
-        self.threads = []
-
-        UDPRcvr = UDPReceive(threadNames[0], self.killFlag,
-                             self.sckt, self.recvQueue)
-        self.threads.append(UDPRcvr)
-        UDPRcvr.start()
-        UDPSend = UDPSender(threadNames[1], self.killFlag,
-                            self.sckt, self.sendQueue)
-        self.threads.append(UDPSend)
-        UDPSend.start()
+        self.UDPRcvr = UDPReceive('UDPRcvr', self.killFlag,
+                                  self.sckt, self.recvQueue)
+        self.UDPRcvr.start()
 
     def send(self, message, dest):
-        toSend = []
-        toSend.append(message)
-        toSend.append(dest)
-        self.sendQueue.put(toSend)
+        maxTries = 3
+        while maxTries > 0:
+            try:
+                self.sckt.sendto(message, dest)
+                break
+            except socket.timeout:
+                maxTries -= 1
+            except socket.error, msg:
+                print >>sys.stderr, "Socket connection error. Code: "\
+                                    + str(msg[0]) + "Message: " + msg[1]
+                raise
+        if maxTries <= 0:
+            raise socket.error
 
     def shutdown(self):
         self.killFlag.set()
-
-        try:
-            self.sendQueue.put_nowait(None)
-        except Queue.Full:
-            pass
-
-        while len(self.threads) > 0:
-            self.threads = [t.join(1) for t in self.threads
-                            if t is not None and t.isAlive()]
+        while self.UDPRcvr.is_alive():
+            self.UDPRcvr.join(1)
 
         self.sckt.close()
 
@@ -83,44 +76,15 @@ class UDPReceive(threading.Thread):
         while 1:
             try:
                 newD = self.sckt.recvfrom(__MAXSIZE__)
-                print self.name + ": Received message from " + str(newD[1])
                 self.recvQueue.put(newD)
             except socket.timeout:
                 pass
             except socket.error, msg:
-                print "Socket connection error. Code: " + str(msg[0]) + \
-                      "Message: " + msg[1]
+                print >>sys.stderr, "Socket connection error. Code: "\
+                                    + str(msg[0]) + "Message: " + msg[1]
                 raise
             finally:
-                if self.killFlag.isSet():
-                    break
-        print self.name + ": Thread exiting"
-
-
-class UDPSender(threading.Thread):
-    """Waits for item to be put in the sendQueue before sending them"""
-
-    def __init__(self, name, killFlag, sckt, sendQueue):
-        super(UDPSender, self).__init__()
-        self.name = name
-        self.killFlag = killFlag
-        self.sckt = sckt
-        self.sendQueue = sendQueue
-
-    def run(self):
-        while 1:
-            try:
-                toSend = self.sendQueue.get()
-                if toSend is not None:
-                    print self.name + ": Sending reply."
-                    self.sckt.sendto(toSend[0], toSend[1])
-            except socket.timeout:
-                self.parent.sendQueue.put(toSend)
-            except socket.error, msg:
-                print "Socket connection error. Code: " + str(msg[0]) + \
-                      "Message: " + msg[1]
-                raise
-            finally:
+                self.recvQueue.put(None)
                 if self.killFlag.isSet():
                     break
         print self.name + ": Thread exiting"
@@ -139,18 +103,21 @@ if __name__ == '__main__':
     try:
         while 1:
             msg = msgQueue.get()
+            if msg is None:
+                continue
             if msg[0] == "Bye!":
                 print "Received disconnection request from: " + str(msg[1])
                 udpserver.send(msg[0], msg[1])
                 break
             else:
                 print "Received message from: " + str(msg[1])
+                print "Sending reply."
                 toSend = "You sent: " + msg[0]
                 udpserver.send(toSend, msg[1])
     except KeyboardInterrupt:
-        pass
+        print "Ctrl-C detected!"
     except socket.error:
-        sys.exit()
+        raise
     finally:
         print "Shutting down UDP Server."
         udpserver.shutdown()
